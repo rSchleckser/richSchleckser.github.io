b75f85c35b4bc278d1ee7e9e5e5e56ba64e514ad
Richard D Schleckser <47959292+rSchleckser@users.noreply.github.com>
2026-09-07 15:01:50 -0700

    Add Experience to footer nav links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -24,6 +24,7 @@ const Footer = () => {
           </a>
           <a href='#about'>About</a>
           <a href='#projects'>Projects</a>
+          <a href='#experience'>Experience</a>
           <a href='#contact'>Contact</a>
         </div>
       </div>
